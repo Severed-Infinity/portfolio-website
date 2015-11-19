21dd448a3b18942ac4526ca72cdec46928037951
David Swift <masterdavidswift@hotmail.com>
2015-11-19 18:58:24 +0000

    Include image of myself, finished about-page content. better styles needed.

--- src/portfolio_website/webpages.clj
+++ src/portfolio_website/webpages.clj
@@ -61,29 +61,67 @@
            [:script {:src "https://oss.maxcdn.com/respond/1.4.2/respond.min.js"}]))]
       [:body
        nav
-       [:div.breaker]
+       [:div.breaker "&#8291;"]
        [:main.container
         more]
-       [:div
+       [:footer.footer.navbar-fixed-bottom
         [:hr]
-        "Icons made by "
-        [:a {:href "http://www.flaticon.com/authors/simpleicon" :title "SimpleIcon"} "SimpleIcon"] " from "
-        [:a {:href "http://www.flaticon.com" :title "Flaticon"} "www.flaticon.com"] "is licensed by "
-        [:a {:href "http://creativecommons.org/licenses/by/3.0/" :title "Creative Commons BY 3.0"} " CC BY 3.0"]]
+        [:p.text-muted
+         "Icons made by "
+         [:a {:href "http://www.flaticon.com/authors/simpleicon" :title "SimpleIcon"} "SimpleIcon"] " from "
+         [:a {:href "http://www.flaticon.com" :title "Flaticon"} "www.flaticon.com"] "is licensed by "
+         [:a {:href "http://creativecommons.org/licenses/by/3.0/" :title "Creative Commons BY 3.0"} " CC BY 3.0"]]]
        (page/include-js
          "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"
          "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js")])))
+
+(def tag-line
+  (hic/html
+    [:aside
+     [:h1 "The up-and-comer"]
+
+     ;;TODO only temporary list
+     #_[:ol
+        [:li "What am I?"]
+        [:li "What do I do?"]
+        [:li "Where am I from? (county/city)"]
+        [:li "Freelancer, working, looking for work?"]]
+
+     [:p "\"Highly intuitive, adept at programming with
+     experience in web development based in Dublin; "
+      [:br]
+      "Seeking to expand and develop new skills while
+      looking for challenging and unique opportunities.\""]
+
+     #_[:p "A programmer and web designer plucked right
+     from the hands of god." [:br]
+        "who loves to build websites and challenge my
+        coding skills." [:br]
+        "living in dublin"
+        [:br]
+        "looking to develop something different and
+        unique"]]))
 
 (def about-page
   (page
     "About - David Swift"
     (hic/html
-      ;tag-line
-      [:section
+      tag-line
+      ;TODO better image of self
+      [:section.col-xs-12.col-md-5
+       [:img {:alt "profile image of me"
+              :src (to-uri "david_swift.jpg")}]]
+      [:section.col-xs-12.col-md-5.col-md-offset-2
        [:h1 "About Me"]
-       [:p " Welcome to the world of David Swift, don’t be afraid to stay awhile, as long as you are here I should introduce myself. Well, I’m a computing student at"
+       [:p " Welcome to the world of David Swift, don’t be afraid to stay awhile, as long as you are here I should introduce myself. Well, I’m a computing student at "
         [:abbr {:title "Institute of Technology Tallaght"} "ITT"]
         ", a fourth to be exact, I’m half way through my final year as of writing this and I’ve learnt quite a bit since entering through the doors of the college."]
+       [:p "My main focus has always been programming, I knew that it was what I’ve wanted to do since finishing my leaving cert, and majority of my experience in
+       programming has been focused on working with JAVA along with this I’ve had a fair years working with html and other web related technologies."]
+       [:p "Most of my knowledge had not come from college work it came from working outside of my comfort zone, researching and reaching out for help, it was no
+       easy task at that but in doing so have come along way and will continue to strive to progress further down these roads."]
+       [:p "I’m known from within my class and some social circles as being very helpful and knowledgable when it comes to figuring out coding problems among other
+       things, giving a helping hand where I can. Always took the leadership role and guided my teams through various projects since my first year and it was never
+       an easy task, many walls stood in the way but ultimately found away over."]
        ]
-      [:img {:alt "profile image of me"
-             :src "#"}])))
+      )))
