f107e8deb0c9d662e94f953df2f5f3969d05bf2e
David Swift <masterdavidswift@hotmail.com>
2015-11-19 23:50:34 +0000

    Setup tab headers and id's, changed the z-index max screen size requirement on navigation

--- src/portfolio_website/projects_page.clj
+++ src/portfolio_website/projects_page.clj
@@ -5,18 +5,18 @@
   (pages/page
     "Projects - David Swift"
     [:ul.nav.nav-tabs
-     [:li.active [:a {:data-toggle "tab" :href "#proj1"} "proj1"]]
-     [:li [:a {:data-toggle "tab" :href "#proj2"} "proj2"]]
-     [:li [:a {:data-toggle "tab" :href "#proj3"} "proj3"]]
+     [:li.active [:a {:data-toggle "tab" :href "#fourth"} "Fourth Year"]]
+     [:li [:a {:data-toggle "tab" :href "#third"} "Third Year"]]
+     [:li [:a {:data-toggle "tab" :href "#second"} "Second Year"]]
      [:li [:a {:data-toggle "tab" :href "#proj4"} "proj4"]]]
     [:div.tab-content
-     [:div#proj1.tab-pane.fade.in.active
+     [:div#fourth.tab-pane.fade.in.active
       [:h3 "proj1"]
       [:p "some text"]]
-     [:div#proj2.tab-pane.fade
+     [:div#third.tab-pane.fade
       [:h3 "proj2"]
       [:p "some text"]]
-     [:div#proj3.tab-pane.fade
+     [:div#second.tab-pane.fade
       [:h3 "proj3"]
       [:p "some text"]]
      [:div#proj4.tab-pane.fade
