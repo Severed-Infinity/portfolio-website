f19e06800dc21f7a1be5f25be6944ab35c6ae4ec
David Swift <masterdavidswift@hotmail.com>
2015-11-19 23:04:05 +0000

    Project page layout structure

--- src/portfolio_website/projects_page.clj
+++ src/portfolio_website/projects_page.clj
@@ -4,7 +4,21 @@
 (def projects
   (pages/page
     "Projects - David Swift"
-    [:section.panel.panel-default
-     [:div.panel-heading
-      [:h2.panel-title "Projects"]]
-     [:article.panel-body "blank"]]))
+    [:ul.nav.nav-tabs
+     [:li.active [:a {:data-toggle "tab" :href "#proj1"} "proj1"]]
+     [:li [:a {:data-toggle "tab" :href "#proj2"} "proj2"]]
+     [:li [:a {:data-toggle "tab" :href "#proj3"} "proj3"]]
+     [:li [:a {:data-toggle "tab" :href "#proj4"} "proj4"]]]
+    [:div.tab-content
+     [:div#proj1.tab-pane.fade.in.active
+      [:h3 "proj1"]
+      [:p "some text"]]
+     [:div#proj2.tab-pane.fade
+      [:h3 "proj2"]
+      [:p "some text"]]
+     [:div#proj3.tab-pane.fade
+      [:h3 "proj3"]
+      [:p "some text"]]
+     [:div#proj4.tab-pane.fade
+      [:h3 "proj4"]
+      [:p "some text"]]]))
