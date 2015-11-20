959e749fba7207449ca050dd7648ea37ae63a619
David Swift <masterdavidswift@hotmail.com>
2015-11-20 23:01:04 +0000

    Added download Icon to CV link

--- src/portfolio_website/education_page.clj
+++ src/portfolio_website/education_page.clj
@@ -1,25 +1,28 @@
 (ns portfolio-website.education-page
-  (:require [hiccup.core :as hic]))
+  (:require [hiccup.core :as hic]
+            [portfolio-website.pages-core :as pages]))
 
 (def education-page
-  (hic/html
-    [:section
-     [:h1 "Education and Results"]
-     [:ul
-      [:li "IT-Tallaght"
-       [:ol
-        [:li "Fourth Year"]
-        [:li "Third Year"]
-        [:li "Second Year"]
-        [:li "First Year"]]]
-      [:li "Pearse College of further education"
-       [:ol
-        [:li "Fetac Level 6"]
-        [:li "Fetac Level 5"]
-        [:li "Repear Leaving Cert"]]]
-      [:li "St. Marks Community School"]]]
-    [:aside
-     [:h1 "Merits and Awards"]
-     [:ul
-      [:li "Zero Days missed"]
-      [:li "Trinity Access Program (TAP)"]]]))
+  (pages/page
+    "Education - David Swift"
+    (hic/html
+      [:section
+       [:h1 "Education and Results"]
+       [:ul
+        [:li "IT-Tallaght"
+         [:ol
+          [:li "Fourth Year"]
+          [:li "Third Year"]
+          [:li "Second Year"]
+          [:li "First Year"]]]
+        [:li "Pearse College of further education"
+         [:ol
+          [:li "Fetac Level 6"]
+          [:li "Fetac Level 5"]
+          [:li "Repear Leaving Cert"]]]
+        [:li "St. Marks Community School"]]]
+      [:aside
+       [:h1 "Merits and Awards"]
+       [:ul
+        [:li "Zero Days missed"]
+        [:li "Trinity Access Program (TAP)"]]])))
--- src/portfolio_website/pages_core.clj
+++ src/portfolio_website/pages_core.clj
@@ -42,7 +42,8 @@
         #_[:li [:a {:href "#"} "Reviews/Comments"]]
         [:li [:a {:href   (to-uri "davidswift_cv.pdf")
                   :target "_blank"
-                  :rel    "nofollow"} "CV"]]]]]]))
+                  :rel    "nofollow"} "CV" [:span.entypo-download]
+              [:img.visible-xs.visbible-sm.img-responsive {:alt "email" :src (to-uri "svg/cloud134.svg")}]]]]]]]))
 
 (defn page [title & more]
   (page/html5
