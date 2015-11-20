5493892b78f503ba667d605ff8c3cb92f14a9f47
David Swift <masterdavidswift@hotmail.com>
2015-11-20 22:12:28 +0000

    Form complete but disabled due to processing errors of emails

--- src/portfolio_website/contact_modal.clj
+++ src/portfolio_website/contact_modal.clj
@@ -12,21 +12,18 @@
         [:button.close {:type "button" :data-dismiss "modal"} "&times;"]
         [:h3 "Email me"]]
        [:div.modal-body
-        [:form.form {:action "MAILTO:masterdavidswift@hotmail.com" :method "post"}
+        [:form.form
          [:div.form-group
-          [:label {:for "email"} "Email"]
-          [:input#email.form-control {:type "email" :placeholder "email@email.com" :required true}]]
+          [:label {:for "eml"} "Email"]
+          [:input#eml.form-control {:type "email" :placeholder "email@email.com" :required true :name "eml"}]]
          [:div.form-group
-          [:label {:for "phone"} "Phone"]
-          [:input#phone.form-control {:type "tel" :value "+353"}]]
+          [:label {:for "phn"} "Phone"]
+          [:input#phn.form-control {:type "tel" :value "+353" :name "phn"}]]
          [:div.form-group
-          [:label {:for "subject"} "Subject"]
-          [:input#subject.form-control {:type "text" :max 40 :placeholder "Subject" :required true}]]
+          [:label {:for "sbj"} "Subject"]
+          [:input#sbj.form-control {:type "text" :max 40 :placeholder "Subject" :required true :name "sbj"}]]
          [:div.form-group
-          [:label {:for "message"} "Message"]
-          [:textarea#message.form-control {:rows 10 :maxlength 1024 :required true :placeholder "Reasons for emailing :)"}]]
+          [:label {:for "mge"} "Message"]
+          [:textarea#mge.form-control {:rows 10 :maxlength 1024 :required true :placeholder "Reasons for emailing :)" :name "mge"}]]
          [:div.form-group
-          [:button.btn.btn-success.col-xs-offset-10 {:type "submit"} "Submit"]]]]
-       [:div.modal-footer
-        ]]
-      ]]))
+          [:button.btn.btn-success.col-xs-offset-10.disabled {:type "submit" :data-toggle "tooltip" :title "disabled due to processing error"} "Submit"]]]]]]]))
