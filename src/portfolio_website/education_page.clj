10605224df8a96a59b0bd1df98ec86b7f342fe9a
David Swift <masterdavidswift@hotmail.com>
2015-11-20 22:37:17 +0000

    Basic education page

---
+++ src/portfolio_website/education_page.clj
@@ -0,0 +1,25 @@
+(ns portfolio-website.education-page
+  (:require [hiccup.core :as hic]))
+
+(def education-page
+  (hic/html
+    [:section
+     [:h1 "Education and Results"]
+     [:ul
+      [:li "IT-Tallaght"
+       [:ol
+        [:li "Fourth Year"]
+        [:li "Third Year"]
+        [:li "Second Year"]
+        [:li "First Year"]]]
+      [:li "Pearse College of further education"
+       [:ol
+        [:li "Fetac Level 6"]
+        [:li "Fetac Level 5"]
+        [:li "Repear Leaving Cert"]]]
+      [:li "St. Marks Community School"]]]
+    [:aside
+     [:h1 "Merits and Awards"]
+     [:ul
+      [:li "Zero Days missed"]
+      [:li "Trinity Access Program (TAP)"]]]))
