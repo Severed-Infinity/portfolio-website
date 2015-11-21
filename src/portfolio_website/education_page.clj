02f37e3dd86381c010e28cda18557db99cebfb25
David Swift <masterdavidswift@hotmail.com>
2015-11-21 00:19:44 +0000

    Education page structure and design complete. TODO content

--- src/portfolio_website/education_page.clj
+++ src/portfolio_website/education_page.clj
@@ -8,21 +8,53 @@
     (hic/html
       [:section
        [:h1 "Education and Results"]
-       [:ul
-        [:li "IT-Tallaght"
-         [:ol
-          [:li "Fourth Year"]
-          [:li "Third Year"]
-          [:li "Second Year"]
-          [:li "First Year"]]]
-        [:li "Pearse College of further education"
-         [:ol
-          [:li "Fetac Level 6"]
-          [:li "Fetac Level 5"]
-          [:li "Repear Leaving Cert"]]]
-        [:li "St. Marks Community School"]]]
-      [:aside
+       [:h2 "IT-Tallaght"]
+       [:div.row
+        [:div#accordian.panel-group.col.xs-12.col-md-6
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4
+            [:a {:data-toggle "collapse" :data-parent "#accordian" :href "#fourthyear"} "Fourth Year"]]]
+          [:div#fourthyear.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4 [:a {:data-toggle "collapse" :data-parent "#accordian" :href "#thirdyear"} "Third Year"]]]
+          [:div#thirdyear.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4 [:a {:data-toggle "collapse" :data-parent "#accordian" :href "#secondyear"} "Second Year"]]]
+          [:div#secondyear.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4 [:a {:data-toggle "collapse" :data-parent "#accordian" :href "#firstyear"} "First Year"]]]
+          [:div#firstyear.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]]]
+       [:h2 "Pearse College of further education"]
+       [:div.row
+        [:div#accordian2.panel-group.col.xs-12.col-md-6
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4
+            [:a {:data-toggle "collapse" :data-parent "#accordian2" :href "#fetaclvl6"} "Fetac Level 6"]]]
+          [:div#fetaclvl6.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4
+            [:a {:data-toggle "collapse" :data-parent "#accordian2" :href "#fetaclvl5"} "Fetac Level 5"]]]
+          [:div#fetaclvl5.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]
+         [:div.panel.panel-default
+          [:div.panel-heading
+           [:h4
+            [:a {:data-toggle "collapse" :data-parent "#accordian2" :href "#repeatyear"} "Repeat Leaving Cert"]]]
+          [:div#repeatyear.panel-collapse.collapse
+           [:div.panel-body "stuff"]]]]]]
+      [:aside.row.container
        [:h1 "Merits and Awards"]
-       [:ul
-        [:li "Zero Days missed"]
-        [:li "Trinity Access Program (TAP)"]]])))
+       [:ul.list-group.col-xs-12.col-md-6
+        [:li.list-group-item "Zero Days missed - Secondary School"]
+        [:li.list-group-item "Trinity Access Program (TAP)"]]])))
